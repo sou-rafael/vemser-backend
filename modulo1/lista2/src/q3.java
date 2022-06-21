5f3450bf3e5e3f2752b51548bf492c734810b01f
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-06-21 02:28:33 -0300

    refatorei a q8, mas nao consegui concluir a q3!

--- modulo1/lista2/src/q3.java
+++ modulo1/lista2/src/q3.java
@@ -3,28 +3,32 @@ import java.util.Scanner;
 public class q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int quantJog = 5;
-        String quit = "SAIR";
+        int quantJog = 1;
         String[][] jogadores = new String[quantJog][4];
 
         // nome - altura - idade - peso
-        for (int i = 0; i < jogadores.length; i++) {
-            do {
-                System.out.println("Jogador " + (i + 1));
+            while(true) {
+                for (int i = 0; i < jogadores.length; i++) {
+                    System.out.println("Jogador " + (i + 1));
+                    System.out.println("Nome: ");
+                    jogadores[i][0] = sc.nextLine();
 
-                System.out.println("Nome: ");
-                jogadores[i][0] = sc.nextLine();
+                    if (!jogadores[i][0].equalsIgnoreCase("SAIR")) {
 
-                System.out.println("Altura: ");
-                jogadores[i][1] = sc.nextLine();
+                        System.out.println("Altura: ");
+                        jogadores[i][1] = sc.nextLine();
 
-                System.out.println("Idade: ");
-                jogadores[i][2] = sc.nextLine();
+                        System.out.println("Idade: ");
+                        jogadores[i][2] = sc.nextLine();
 
-                System.out.println("Peso: ");
-                jogadores[i][3] = sc.nextLine();
-
-            } while (!jogadores[i][0].equalsIgnoreCase(quit));
-        }
+                        System.out.println("Peso: ");
+                        jogadores[i][3] = sc.nextLine();
+                        quantJog++;
+                    } else if (jogadores[i][0].equalsIgnoreCase("SAIR")) {
+                        break;
+                    }
+                }
+            }
     }
+
 }
--- modulo1/lista2/src/q8.java
+++ modulo1/lista2/src/q8.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class q8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        int[][] notas = new int[5][4];
         double[][] notas = {{100, 8, 2, 0},{200,7,1,0},{300,8,4,0},{400,5,6,0},{500,9,6,0}};
+
+//        int[][] notas = new int[5][4];
 
 //        for (int i = 0; i < notas.length; i++) {
 //            System.out.println("Aluno " + (i + 1));
@@ -31,16 +32,16 @@ public class q8 {
             System.out.println(notas[i][0]+" | "+notas[i][1]+" | "+notas[i][2]);
             notas[i][3] = notas[i][1]*0.6 + notas[i][2]*0.4;
         }
+
         // media da notas finais
         double somaNF = 0;
         for(double[] elem:notas){
             somaNF += elem[3];
         }
         double mediaNF = somaNF/ notas.length;
-
         System.out.println("Media das notas finais "+mediaNF);
 
-//        Buscar a matricula de maior nota final
+        // Buscar a matricula de maior nota final
 
         double maior = 0;
         int indiceMaior = -1;
@@ -51,7 +52,6 @@ public class q8 {
             }
         }
         System.out.println("Maior nota eh "+maior+", na matricula "+notas[indiceMaior][0]);
-
 
     }
 }
