89dbc16faae5bf0150670d93558b5b813b8e504a
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-06-21 10:58:53 -0300

    q3 corrigida

--- modulo1/lista2/src/q3.java
+++ modulo1/lista2/src/q3.java
@@ -4,17 +4,50 @@ public class q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String nome = "";
-        double altura, somaAltura = 0, mediaAltura = 0, idade, maiorIdade = 0, peso, maiorPeso = 0;
-        while(!nome.equalsIgnoreCase("SAIR")){
+        String nome = "", nomeMaisVelho = "", nomeMaisPesado = "";
+        float altura = 0.0f, maiorAltura = 0.0f, somaAltura = 0.0f, mediaAltura = 0.0f, peso, maiorPeso = 0.0f;
+        int cont = 0, idade, maiorIdade = 0;
+
+        while(true){
+            cont++;
             System.out.println("Nome:");
             nome = sc.nextLine();
+            if(nome.equalsIgnoreCase("SAIR")) {
+                break;
+            }
 
             System.out.println("Insira a altura:");
-            altura = sc.nextDouble();
+            altura = sc.nextFloat();
+            sc.nextLine();
             somaAltura = somaAltura + altura;
+            if(altura > maiorAltura){
+                maiorAltura = altura;
+            }
 
+            System.out.println("Idade:");
+            idade = sc.nextInt();
+            sc.nextLine();
+            if(idade > maiorIdade){
+                maiorIdade = idade;
+                nomeMaisVelho = nome;
+            }
+
+            System.out.println("Peso");
+            peso = sc.nextFloat();
+            sc.nextLine();
+            if(peso > maiorPeso){
+                maiorPeso = peso;
+                nomeMaisPesado = nome;
+            }
 
         }
+        mediaAltura = somaAltura/cont;
+        System.out.println("Quantidade de jogadores: "+cont);
+        System.out.println("Altura do maior jogador eh"+maiorAltura);
+        System.out.println("O jogador mais velho eh "+nomeMaisVelho+" com "+maiorIdade+" anos.");
+        System.out.println("O jogador mais pesado eh "+nomeMaisPesado+" com "+maiorPeso+" kilos.");
+
+
+
     }
 }
