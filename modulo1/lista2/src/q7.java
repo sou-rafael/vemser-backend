9a3eeddc7b2462120a74675ecc858ed953f5ece9
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-06-22 09:13:57 -0300

    corrigido q7 e q8 - lendo inform do usuario

--- modulo1/lista2/src/q7.java
+++ modulo1/lista2/src/q7.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class q7 {
     public static void main(String[] args) {
-        int[][] matriz = {{0,1,2,3}, {10,11,12,13}, {20,21,22,23}, {30,31,32,33}};
+//        int[][] matriz = {{0,1,2,3}, {10,11,12,13}, {20,21,22,23}, {30,31,32,33}};
         int cont = 0;
+        Scanner sc = new Scanner(System.in);
+        int[][] matriz = new int[4][4];
+
+//        Criar a lista de valores
+        for(int i = 0; i<= matriz.length-1; i++) {
+            for(int j = 0; j <= matriz.length-1; j++) {
+                System.out.println("Insira o valor na linha "+i+", coluna "+j);
+                matriz[i][j] = sc.nextInt();
+                sc.nextLine();
+            }
+        }
 
 //         matriz [i][j]
         for(int i = 0; i < matriz.length; i++){
--- modulo1/lista2/src/q8.java
+++ modulo1/lista2/src/q8.java
@@ -3,55 +3,58 @@ import java.util.Scanner;
 public class q8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double[][] notas = {{100, 8, 2, 0},{200,7,1,0},{300,8,4,0},{400,5,6,0},{500,9,6,0}};
+//        double[][] notas = {{100, 8, 2, 0},{200,7,1,0},{300,8,4,0},{400,5,6,0},{500,9,6,0}};
 
-//        int[][] notas = new int[5][4];
+        double[][] notas = new double[2][4];
 
-//        for (int i = 0; i < notas.length; i++) {
-//            System.out.println("Aluno " + (i + 1));
-//
-//            System.out.println("Numero da Matricula: ");
-//            notas[i][0] = sc.nextInt();
-//            sc.nextLine();
-//
-//            System.out.println("Media das Provas: ");
-//            notas[i][1] = sc.nextInt();
-//            sc.nextLine();
-//
-//            System.out.println("Media dos Trabalhos: ");
-//            notas[i][2] = sc.nextInt();
-//            sc.nextLine();
-//
-//            System.out.println("Nota Final: ");
-//            notas[i][3] = sc.nextInt();
-//            sc.nextLine();
-//        }
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Aluno " + (i + 1));
+
+            System.out.println("Numero da Matricula: ");
+            notas[i][0] = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("Media das Provas: ");
+            notas[i][1] = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("Media dos Trabalhos: ");
+            notas[i][2] = sc.nextInt();
+            sc.nextLine();
+
+            double somaNF = notas[i][1] + notas[i][2];
+            notas[i][3] = somaNF;
+//                System.out.println("Nota Final: ");
+
+        }
 
         // ler as 3 primeiras informações e ja calcular a nota final
-        for(int i = 0; i < notas.length; i++){
-            System.out.println(notas[i][0]+" | "+notas[i][1]+" | "+notas[i][2]);
-            notas[i][3] = notas[i][1]*0.6 + notas[i][2]*0.4;
+        System.out.println("*****************************************\nExibindo as 3 primeiras informacoes:");
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println(notas[i][0] + " | " + notas[i][1] + " | " + notas[i][2]);
+            notas[i][3] = notas[i][1] * 0.6 + notas[i][2] * 0.4;
         }
+        System.out.println("****************************************");
 
         // media da notas finais
-        double somaNF = 0;
-        for(double[] elem:notas){
-            somaNF += elem[3];
+        double somaNFTotal = 0;
+        for (double[] elem : notas) {
+            somaNFTotal += elem[3];
         }
-        double mediaNF = somaNF/ notas.length;
-        System.out.println("Media das notas finais "+mediaNF);
+        double mediaNF = somaNFTotal / notas.length;
+        System.out.println("Media das notas finais " + mediaNF);
 
-        // Buscar a matricula de maior nota final
+            // Buscar a matricula de maior nota final
 
-        double maior = 0;
-        int indiceMaior = -1;
-        for (int i = 0; i < notas.length; i++) {
-            if (notas[i][3] > maior) {
-                maior = notas[i][3];
-                indiceMaior = i;
+            double maior = 0;
+            int indiceMaior = -1;
+            for (int i = 0; i < notas.length; i++) {
+                if (notas[i][3] > maior) {
+                    maior = notas[i][3];
+                    indiceMaior = i;
+                }
             }
-        }
-        System.out.println("Maior nota eh "+maior+", na matricula "+notas[indiceMaior][0]);
+            System.out.println("Maior nota eh " + maior + ", na matricula " + notas[indiceMaior][0]);
 
+        }
     }
-}
