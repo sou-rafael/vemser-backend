5caa631686b33bb2215d4ddd1e4883706f134ffe
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-07-14 15:29:46 -0300

    ajustes completos

--- src/main/java/br/com/vemser/pessoaapi/service/DadosPessoaisService.java
+++ src/main/java/br/com/vemser/pessoaapi/service/DadosPessoaisService.java
@@ -3,19 +3,14 @@ package br.com.vemser.pessoaapi.service;
 import br.com.vemser.pessoaapi.client.DadosPessoaisClient;
 import br.com.vemser.pessoaapi.dto.DadosPessoaisDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
-
+@RequiredArgsConstructor
 public class DadosPessoaisService implements DadosPessoaisClient {
 
     private final DadosPessoaisClient dadosPessoaisClient;
-
-    public DadosPessoaisService(DadosPessoaisClient dadosPessoaisClient) {
-        this.dadosPessoaisClient = dadosPessoaisClient;
-    }
 
     @Override
     public List<DadosPessoaisDTO> getAll() {
