e598c12b324f14f27c899424a915467a2e1a9cdb
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-07-19 09:09:23 -0300

    corrigido configs

--- modulo3_2/pessoa-api/src/main/java/br/com/vemser/pessoaapi/entity/PessoaEnderecoEntity.java
+++ modulo3_2/pessoa-api/src/main/java/br/com/vemser/pessoaapi/entity/PessoaEnderecoEntity.java
@@ -5,11 +5,13 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Getter
 @Setter
 @Entity(name = "PESSOA_X_PESSOA_ENDERECO")
 public class PessoaEnderecoEntity {
+    @Id
     @Column(name = "ID_PESSOA")
     private Integer idPessoa;
     @Column(name = "ID_ENDERECO")
