da78dd7b24df806c32399b3f30612522151ef257
Rafael Ferreira <ferreiradrrafael@gmail.com>
2022-07-26 00:07:55 -0300

    HW aula 1 mod3_3

--- modulo3_3/datas/src/main.java
+++ modulo3_3/datas/src/main.java
@@ -5,84 +5,80 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
 import static java.time.temporal.ChronoUnit.DAYS;
 import static java.time.temporal.ChronoUnit.MONTHS;
 
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        LocalDate hj = LocalDate.now();
-//
-//        System.out.print("Digite a data do seu nascimento: ");
-//        String data = sc.nextLine();
-//
-//        LocalDate aniv = convertToLocalDate(data);
-//        // caso a data seja digitada errada  ele retorna null - usar isso num while
-//
-//        LocalDate localDateAniv = LocalDate.of(hj.getYear(), aniv.getMonthValue(), aniv.getDayOfMonth());
-//        LocalDate localDateHj = hj;
-//
-//        if (localDateAniv.isBefore(localDateHj)) {
-//            Period periodo = Period.between(localDateAniv, localDateHj);
-//            System.out.println("Faltam\n" +
-//                    periodo.getYears() + " Anos\n" +
-//                    periodo.getMonths() + " Meses\n" +
-//                    periodo.getDays() + " Dias\n" +
-//                    "para o seu aniversario.");
-//        } else {
-//            Period periodo = Period.between(localDateHj, localDateAniv);
-//            System.out.println("Faltam\n" +
-//                    periodo.getYears() + " Anos\n" +
-//                    periodo.getMonths() + " Meses\n" +
-//                    periodo.getDays() + " Dias\n" +
-//                    "para o seu aniversario.");
-//        }
-////  ==========================================================================================================
-//        System.out.print("Digite a primeira data: ");
-//        String data1 = sc.nextLine();
-//
-//        System.out.print("Digite a segunda data: ");
-//        String data2 = sc.nextLine();
-//
-//        LocalDate data1LocalDate = convertToLocalDate(data1);
-//        LocalDate data2LocalDate = convertToLocalDate(data2);
-//
-//        if (data1LocalDate.isBefore(data2LocalDate)) {
-//            Period periodo = Period.between(localDateAniv, localDateHj);
-//            System.out.println(periodo.getYears() + " Anos " + periodo.getMonths() + " Meses " + periodo.getDays() + " Dias");
-//        } else {
-//            Period periodo = Period.between(localDateHj, localDateAniv);
-//            System.out.println(periodo.getYears() + " Anos " + periodo.getMonths() + " Meses " + periodo.getDays() + " Dias");
-//        }
-////  ===========================================================================================================
-//        LocalDateTime hoje = LocalDateTime.now();
-//        LocalDateTime futuro = hoje.plusDays(15).plusHours(10);
-//        System.out.println("Dia da semana: "+futuro.getDayOfWeek()+
-//                "Dia do ano: "+futuro.getDayOfYear());
-////  ===========================================================================================================
+        LocalDate hj = LocalDate.now();
+
+        System.out.print("Digite a data do seu nascimento: ");
+        String data = sc.nextLine();
+
+        LocalDate aniv = convertToLocalDate(data);
+
+        LocalDate localDateAniv = LocalDate.of(hj.getYear(), aniv.getMonthValue(), aniv.getDayOfMonth());
+        LocalDate localDateHj = hj;
+
+        if (localDateAniv.isBefore(localDateHj)) {
+            Period periodo = Period.between(localDateAniv, localDateHj);
+            System.out.println("Faltam\n" +
+                    periodo.getYears() + " Anos\n" +
+                    periodo.getMonths() + " Meses\n" +
+                    periodo.getDays() + " Dias\n" +
+                    "para o seu aniversario.");
+        } else {
+            Period periodo = Period.between(localDateHj, localDateAniv);
+            System.out.println("Faltam\n" +
+                    periodo.getYears() + " Anos\n" +
+                    periodo.getMonths() + " Meses\n" +
+                    periodo.getDays() + " Dias\n" +
+                    "para o seu aniversario.");
+        }
+//  ==========================================================================================================
+        System.out.print("Digite a primeira data: ");
+        String data1 = sc.nextLine();
+
+        System.out.print("Digite a segunda data: ");
+        String data2 = sc.nextLine();
+
+        LocalDate data1LocalDate = convertToLocalDate(data1);
+        LocalDate data2LocalDate = convertToLocalDate(data2);
+
+        if (data1LocalDate.isBefore(data2LocalDate)) {
+            Period periodo = Period.between(localDateAniv, localDateHj);
+            System.out.println(periodo.getYears() + " Anos " + periodo.getMonths() + " Meses " + periodo.getDays() + " Dias");
+        } else {
+            Period periodo = Period.between(localDateHj, localDateAniv);
+            System.out.println(periodo.getYears() + " Anos " + periodo.getMonths() + " Meses " + periodo.getDays() + " Dias");
+        }
+//  ===========================================================================================================
+        LocalDateTime hoje = LocalDateTime.now();
+        LocalDateTime futuro = hoje.plusDays(15).plusHours(10);
+        System.out.println("Dia da semana: "+futuro.getDayOfWeek()+
+                "Dia do ano: "+futuro.getDayOfYear());
+//  ===========================================================================================================
 
         ZoneId fusoHorarioDeLondres = ZoneId.of("Europe/London");
-        LocalDateTime dataDoEventoEmLondres = LocalDateTime.of(2024,9,14,18,30);
+        LocalDateTime dataDoEventoEmLondres = LocalDateTime.of(2024, 9, 14, 18, 30);
 
-        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime nowInLondon = LocalDateTime.now(fusoHorarioDeLondres);
 
-        long nowMili = now.atZone(ZoneId.systemDefault())
-                .toInstant() // Instant object
-                .toEpochMilli(); // milisegundos ja no fuso de londres
-        long dataDoEventoMili = dataDoEventoEmLondres.atZone(fusoHorarioDeLondres)
-                .toInstant() // Instant object
-                .toEpochMilli(); // milisegundos ja no fuso de londres
+        int anos = dataDoEventoEmLondres.getYear() - nowInLondon.getYear();
+        int meses = abs(dataDoEventoEmLondres.getMonthValue() - nowInLondon.getMonthValue());
+        int dias = abs(dataDoEventoEmLondres.getDayOfMonth() - nowInLondon.getDayOfMonth());
+        int horas = abs(dataDoEventoEmLondres.getHour() - nowInLondon.getHour());
+        int minutos = abs(dataDoEventoEmLondres.getMinute() - nowInLondon.getMinute());
+        int segundos = abs(dataDoEventoEmLondres.getSecond() - nowInLondon.getSecond());
 
-        long diferenca = dataDoEventoMili - nowMili;
-        Instant instant = Instant.ofEpochMilli(diferenca);
-        LocalDateTime diff = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-        System.out.println(diff.getYear() + " Anos "+
-                        diff.getMonthValue() + " Meses "+
-                        diff.getDayOfMonth() + " Dias "+
-                diff.getHour()+ " Horas "+
-                diff.getMinute()+ "Minutos "+
-                diff.getSecond()+ "Segundos");
-
+        System.out.println(anos + " Anos " +
+                meses + " Meses " +
+                dias + " Dias " +
+                horas + " Horas " +
+                minutos + " Minutos " +
+                segundos + " Segundos");
     }
 
     public static LocalDate convertToLocalDate(String date) {
@@ -95,15 +91,4 @@ public class main {
         }
         return null;
     }
-
-//    public static LocalDateTime convertToLocalDateTimeViaMilisecond(long dateToConvert) {
-//        LocalDateTime.
-//        return Instant.ofEpochMilli(dateToConvert).to;
-//    }
 }
-//        long anos = ChronoUnit.YEARS.between(nowInLondres,dataDoEventoEmLondres);
-//        long meses = ChronoUnit.MONTHS.between(dataDoEventoEmLondres,nowInLondres);
-//        long dias = ChronoUnit.DAYS.between(dataDoEventoEmLondres,nowInLondres);
-//        long horas = ChronoUnit.HOURS.between(dataDoEventoEmLondres,nowInLondres);
-//        long minutos = ChronoUnit.MINUTES.between(dataDoEventoEmLondres,nowInLondres);
-//        long segundos = ChronoUnit.SECONDS.between(dataDoEventoEmLondres,nowInLondres);
